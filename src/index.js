bede4d13c0c4182d10c481897a7b1528d5c3c304
Lichunlin <504234675@qq.com>
2020-04-04 18:55:24 +0800

    3-3：webpack打包静态样式上

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import photo from './img1.jpg';
+import './index.scss';
 
 var img = new Image();
 img.src = photo;
+img.classList.add('photo');
 
 var root = document.getElementById('root');
 root.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,35 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   module: {
-    rules: [{
-      test: /\.jpg$/,
-      use: {
-        loader: 'url-loader', 
-        options: {
-          name: '[name]_[hash].[ext]', // 输出文件名称配置
-          outputPath: './images', // 输出路径配置
-          limit: 102400
-        }
+    rules: [
+      {
+        test: /\.jpg$/,
+        use: {
+          loader: 'url-loader', 
+          options: {
+            name: '[name]_[hash].[ext]', // 输出文件名称配置
+            outputPath: './images', // 输出路径配置
+            // publicPath: 'assets/images',
+            limit: 10240
+          }
+        },
+      }, { // webpack打包时，找到命中的文件后，使用loader编译文件时，use中的loader是倒序编译的，如scss编译时的执行顺序为：sass-loader，css-loader，style-loader
+        test: /(\.scss$)|\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              // importLoader: 2 为了让scss文件内部引用的scss文件打包时，也从postcss-loader开始执行。
+              // 如果不配置，可能直接scss文件内部引用的scss文件会直接从css-loader开始执行，从而导致scss文件内部引用的scss文件样式不会被编译成浏览器可识别的css语法
+              importLoader: 2,
+            }
+          },
+          'sass-loader',
+          'postcss-loader',
+        ]
       },
-    }],
+    ],
   },
   output: {
     filename: 'bundle.js',
