d28e08da83e68e767ce31b92069c166233c7875a
Lichunlin <504234675@qq.com>
2020-04-06 13:33:08 +0800

    3-5:导报过程中使用plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -43,8 +45,14 @@ module.exports = {
       },
     ],
   },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   }
 }
